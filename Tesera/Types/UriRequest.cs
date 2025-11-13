cb8481150e2784cb255fbbf4a5f6166711f90ccb
Vasilii <pimenovasya@gmail.com>
2025-11-13 15:53:24 +0300

    Fixed public virtual method GetUri

--- Tesera/Types/UriRequest.cs
+++ Tesera/Types/UriRequest.cs
@@ -2,6 +2,6 @@
 {
 	public abstract class UriRequest
 	{
-		public virtual string GetUri() => $"https://api.tesera.ru/{this.GetType().FullName?.Remove(0, "Tesera.API".Length).Replace('.', '/').Replace('+', '/')}";
+		public virtual string GetUri() => $"https://www.tesera.ru{this.GetType().FullName?.Remove(0, "Tesera.API".Length).Replace('.', '/').Replace('+', '/')}";
 	}
 }
